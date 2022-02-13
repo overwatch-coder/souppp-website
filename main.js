33ba67dc870477b0952f38574c848200ac1b0448
brainy <devbrainy98@gmail.com>
2022-02-13 22:07:52 +0100

    modified codes

--- main.js
+++ main.js
@@ -8,20 +8,33 @@ closeMenu.addEventListener('click', hideNavbar);
 
 function displayNavbar(){
     orderHeader.style.backgroundColor = "rgb(251, 251, 251, 1)";
+    if(document.documentElement.clientWidth >= 760){
+        orderHeader.style.width = "100%";
+        navbar.style.display = "block";
+    }
+    
+    orderHeader.style.width = "100%";
+    orderHeader.style.transition = "0.5s";
     navbar.style.display = "block";
     displayOrHideProps(openMenu, closeMenu);
 }
 
 function hideNavbar(){
     orderHeader.style.backgroundColor = "transparent";
-    navbar.style.display = "none";
+    if(document.documentElement.clientWidth >= 760){
+        orderHeader.style.width = "100%";
+        navbar.style.display = "block";
+    }else{
+        orderHeader.style.width = "0";
+        navbar.style.display = "none";
+    }
+    orderHeader.style.transition = "0.5s";
     displayOrHideProps(closeMenu, openMenu);
 }
 
 function displayOrHideProps(param1, param2) {
     param1.classList.remove('d-block');
     param1.classList.add('d-none');
-
     param2.classList.remove('d-none');
     param2.classList.add('d-block');
 }
